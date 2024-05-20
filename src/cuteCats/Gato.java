bcdf00a8fd63ba8b18e7f8f335c21003f6877956
Beat02 <beatriz.ruiz7@educa.madrid.org>
2024-05-20 11:00:08 +0200

    Gato clase normal, metodos principales terminados

--- src/cuteCats/Gato.java
+++ src/cuteCats/Gato.java
@@ -1,6 +1,6 @@
 package cuteCats;
 
-public abstract class Gato {
+public class Gato {
     protected String nombre;
     protected String etapa;
     protected String color;
@@ -10,14 +10,27 @@ public abstract class Gato {
         this.etapa=etapa;
         this.color = color;
     }
-    public abstract void miau(String color);
-    public void zoomies(String etapa){
-        if (etapa.equalsIgnoreCase(Constante.LITTLEBABY)){
-            System.out.println();
-        } else if (etapa.equalsIgnoreCase(Constante.BABY)) {
-
-        } else if (etapa.equalsIgnoreCase(Constante.BIGBABY)) {
-
+    public  void miau(String color){
+        if (color.equalsIgnoreCase(Constante.COLORNARANJA)) {
+            System.out.println("MIIIIIAAAAAUUUUUUU (está mirando a una esquina, probablemente haya un fantasma)");
+        } else if (color.equalsIgnoreCase(Constante.COLORBLANCO)) {
+            System.out.println("miauuu (con acento francés)");
+        } else if (color.equalsIgnoreCase(Constante.COLORNEGRO)) {
+            System.out.println("MrrrIIIrrrAArrrUUU (te trae un juguete para que se lo lances)");
+        } else if (color.equalsIgnoreCase(Constante.COLORCAREY)) {
+            System.out.println("miiiiiiaaaaauuuu miaaaaaaaauuuuu (te está pidiendo comida)");
+        }else {
+            System.out.println("ฅ^•ﻌ•^ฅ");
         }
     }
+    public void zoomies(String etapa){
+        if (etapa.equalsIgnoreCase(Constante.LITTLEBABY)){
+            System.out.println("Tu gatito tiene tanta energía que ha tirado el jarrón de la mesa, y un par de tazas");
+        } else if (etapa.equalsIgnoreCase(Constante.BABY)) {
+            System.out.println("No te sorprendas si te salta encima alguna que otra noche, sigué siendo un bebé");
+        } else if (etapa.equalsIgnoreCase(Constante.BIGBABY)) {
+            System.out.println("No importa lo mayor que sea, los treats le siguen dando zoomies");
+        }
+    }
+    public void
 }

f70c4962cb06774532a582d2f0474e64dbe7591d
Beat02 <beatriz.ruiz7@educa.madrid.org>
2024-05-20 11:10:46 +0200

    trabajando en el main

--- src/cuteCats/Main.java
+++ src/cuteCats/Main.java
@@ -1,4 +1,14 @@
 package cuteCats;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int numeroGatos;
+        System.out.println("Bienvenide, por fín has conseguido tu objetivo en la vida, ser la señora de los gatos" +
+                '\n' + "Dime, ¿cuantos gatos quieres tener?");
+        numeroGatos = teclado.nextInt();
+
+    }
 }
